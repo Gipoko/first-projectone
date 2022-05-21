b20ea661bf9a92877fc868c84ea45b99a3d930da
arjay <arjayjay.jc@gmail.com>
2022-05-22 00:13:28 +0800

    s

--- resources/views/user/userdash.blade.php
+++ resources/views/user/userdash.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<!-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard for user') }}
@@ -14,4 +14,17 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> 
