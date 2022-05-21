498fe6e0871edcf97a60c4c846d10a0deb6e25e7
arjay <arjayjay.jc@gmail.com>
2022-05-21 22:10:40 +0800

    d

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//auth route for both 
+Route::group(['middleware' => ['auth']], function() { 
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+});
+
+// for users
+Route::group(['middleware' => ['auth', 'role:user']], function() { 
+    Route::get('/dashboard/myprofile', 'App\Http\Controllers\DashboardController@myprofile')->name('dashboard.myprofile');
+});
+
+// for blogwriters
+Route::group(['middleware' => ['auth', 'role:admin']], function() { 
+    Route::get('/dashboard/postcreate', 'App\Http\Controllers\DashboardController@postcreate')->name('dashboard.postcreate');
+    Route::get('/dashboard/book', 'App\Http\Controllers\DashboardController@book')->name('dashboard.book');
+    
+    Route::resource('books', BooksController::class);
+});
+
+
+require __DIR__.'/auth.php';
